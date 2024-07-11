ebba9de3dcd698268f66d6f30a9cb0ec218bd641
Ozodbek2129 <ozodbekshamsiddinov1504@gmail.com>
2024-07-11 11:26:30 +0500

    Reservation logger file

--- service/reservation.go
+++ service/reservation.go
@@ -3,26 +3,31 @@ package service
 import (
 	"context"
 	"database/sql"
-	"log"
+	"fmt"
+	"log/slog"
 	pb "reservation/genproto/resirvation"
+	"reservation/pkg/logger"
 	"reservation/storage/postgres"
 )
 
 type ReservationService struct {
 	pb.UnimplementedResirvationServer
-	Reser *postgres.Reservation
+	Reser  *postgres.Reservation
+	Logger *slog.Logger
 }
 
 func NewReservationService(db *sql.DB) *ReservationService {
 	reser := postgres.NewReservationRepo(db)
 	return &ReservationService{
-		Reser: reser}
+		Reser:  reser,
+		Logger: logger.NewLogger(),
+	}
 }
 
 func (r *ReservationService) Createreservations(ctx context.Context, req *pb.RequestReservations) (*pb.Status, error) {
 	resp, err := r.Reser.CreateReservation(req)
 	if err != nil {
-		log.Fatalf("Malumotlarni insert qilishda xatolik: %v", err)
+		r.Logger.Error(fmt.Sprintf("Malumotlarni insert qilishda xatolik: %v", err))
 		return nil, err
 	}
 
@@ -32,7 +37,7 @@ func (r *ReservationService) Createreservations(ctx context.Context, req *pb.Req
 func (r *ReservationService) GetAllReservations(ctx context.Context, req *pb.Void) (*pb.Reservations, error) {
 	resp, err := r.Reser.GetAllReservation()
 	if err != nil {
-		log.Fatalf("Malumotlarni olishda xatolik: %v", err)
+		r.Logger.Error(fmt.Sprintf("Malumotlarni olishda xatolik: %v", err))
 		return nil, err
 	}
 	return resp, nil
@@ -41,7 +46,7 @@ func (r *ReservationService) GetAllReservations(ctx context.Context, req *pb.Voi
 func (r *ReservationService) GetByIdReservations(ctx context.Context, req *pb.ReservationId) (*pb.Reservation, error) {
 	resp, err := r.Reser.GetReservationByID(req)
 	if err != nil {
-		log.Fatalf("Bitta malumotni olishda xatolik: %v", err)
+		r.Logger.Error(fmt.Sprintf("Bitta malumotni olishda xatolik: %v", err))
 		return nil, err
 	}
 	return resp, nil
@@ -50,7 +55,7 @@ func (r *ReservationService) GetByIdReservations(ctx context.Context, req *pb.Re
 func (r *ReservationService) UpdateReservations(ctx context.Context, req *pb.ReservationUpdate) (*pb.Status, error) {
 	resp, err := r.Reser.UpdateReservations(req)
 	if err != nil {
-		log.Fatalf("Malumotlarni update qilishda xatolik: %v", err)
+		r.Logger.Error(fmt.Sprintf("Malumotlarni update qilishda xatolik: %v", err))
 		return nil, err
 	}
 	return resp, nil
@@ -59,7 +64,7 @@ func (r *ReservationService) UpdateReservations(ctx context.Context, req *pb.Res
 func (r *ReservationService) DeleteReservations(ctx context.Context, req *pb.ReservationId) (*pb.Status, error) {
 	resp, err := r.Reser.DeleteReservation(req)
 	if err != nil {
-		log.Fatalf("Resirvation deleting error: %v", err)
+		r.Logger.Error(fmt.Sprintf("Resirvation deleting error: %v", err))
 		return nil, err
 	}
 	return resp, nil
@@ -68,7 +73,7 @@ func (r *ReservationService) DeleteReservations(ctx context.Context, req *pb.Res
 func (r *ReservationService) GetReservationsByUserId(ctx context.Context, req *pb.UserId) (*pb.Reservations, error) {
 	resp, err := r.Reser.GetReservationsByUserId(req)
 	if err != nil {
-		log.Fatalf("User id buyicha malumotlarni olishda xatolik: %v", err)
+		r.Logger.Error(fmt.Sprintf("User id buyicha malumotlarni olishda xatolik: %v", err))
 		return nil, err
 	}
 	return resp, nil
@@ -77,7 +82,7 @@ func (r *ReservationService) GetReservationsByUserId(ctx context.Context, req *p
 func (r *ReservationService) OrderMeal(ctx context.Context, req *pb.Order) (*pb.Status, error) {
 	resp, err := r.Reser.OrderMeal(req)
 	if err != nil {
-		log.Fatalf("Order service da xatolik: %v", err)
+		r.Logger.Error(fmt.Sprintf("Order service da xatolik: %v", err))
 		return nil, err
 	}
 	return resp, nil
@@ -86,7 +91,7 @@ func (r *ReservationService) OrderMeal(ctx context.Context, req *pb.Order) (*pb.
 func (r *ReservationService) PayForReservation(ctx context.Context, req *pb.Payment) (*pb.Status, error) {
 	resp, err := r.Reser.PayForReservation(req)
 	if err != nil {
-		log.Fatalf("Payment service da xatolik: %v", err)
+		r.Logger.Error(fmt.Sprintf("Payment service da xatolik: %v", err))
 		return nil, err
 	}
 	return resp, nil
